f3d8e5d2a29c68329263543f4890936a1afac196
Maddduru Balaji <balajimadduru12>
2025-01-03 22:52:48 +0530

    changed the appBar background color

--- lib/practice.dart
+++ lib/practice.dart
@@ -4,14 +4,16 @@ import 'package:flutter/material.dart';
 class Test extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-     return MaterialApp(
-       home: Scaffold(
-         appBar: AppBar(
-           backgroundColor: Colors.deepOrangeAccent,
-           title: Text("This is a App Bar"),
+      return MaterialApp(
+         home: Scaffold(
+           appBar: AppBar(
+             backgroundColor: Colors.green,
+             title: Text(
+               "App Bar"
+             ),
+           ),
          ),
-       ),
-     );
+      );
   }
 
 }
