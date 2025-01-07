00b49f406d4d50627036416c66f89e6bc3d7ed27
Maddduru Balaji <balajimadduru12>
2025-01-07 23:59:38 +0530

    modified screen navigation

--- lib/navigation/screenNavigation.dart
+++ lib/navigation/screenNavigation.dart
@@ -2,29 +2,36 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(First());
+  runApp(MyApp()); // Use the MyApp widget to wrap the MaterialApp
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: First(),  // Set First screen as home
+    );
+  }
 }
 
 class First extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.green,
-          title: Text("First Screen"),
-        ),
-        body: Center(
-          child: ElevatedButton(
-            onPressed: () {
-              Navigator.push(context, MaterialPageRoute(builder: (context) {return Second();}));
-              /*Alternative way of using navigation */
-              // Navigator.push(context, MaterialPageRoute(builder: (context) => Second()),);
-            },
-            child: const Text(
-              "Click Me",
-              style: TextStyle(color: Colors.black),
-            ),
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.green,
+        title: Text("First Screen"),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.push(context, MaterialPageRoute(builder: (context) => Second()),);
+            /*Alternative way of using navigation */
+            // Navigator.push(context, MaterialPageRoute(builder: (context) => Second()),);
+          },
+          child: const Text(
+            "Click Me",
+            style: TextStyle(color: Colors.black),
           ),
         ),
       ),
